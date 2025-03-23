b42fba7037304c68c79141392d37bce5eb42a500
Ritik Mangal <107031511+RITIKMANGAL@users.noreply.github.com>
2025-03-23 23:53:53 +0530

    second

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://netflix-2-0-2se7.onrender.com",
       }
     }
   }
