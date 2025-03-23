a9db5f96b8092b7720ad4ae597a8394181cbcee8
Ritik Mangal <107031511+RITIKMANGAL@users.noreply.github.com>
2025-03-23 23:42:01 +0530

    first

---
+++ backend/routes/search.route.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+	getSearchHistory,
+	removeItemFromSearchHistory,
+	searchMovie,
+	searchPerson,
+	searchTv,
+} from "../controllers/search.controller.js";
+
+const router = express.Router();
+
+router.get("/person/:query", searchPerson);
+router.get("/movie/:query", searchMovie);
+router.get("/tv/:query", searchTv);
+
+router.get("/history", getSearchHistory);// to fetch the user search history
+
+router.delete("/history/:id", removeItemFromSearchHistory);// to delete the user search history.
+
+export default router;
